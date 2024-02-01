4e52d2d932a369c8e1bb20bf2c126b51f7014e0b
QuanNB <quannb@vnpt.vn>
2024-02-01 08:56:20 +0700

    up

--- modules/Comments/Routes/web.php
+++ modules/Comments/Routes/web.php
@@ -1,21 +1,2 @@
 <?php
 
-<<<<<<< HEAD
-=======
-use Digisource\Settings\Http\Controllers\V1\SettingsCandidateController;
-use Digisource\Settings\Http\Controllers\V1\SettingsLocationsController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-*/
-
->>>>>>> c56245a (up)
